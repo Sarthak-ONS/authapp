ce529718a8ddc1c77f0ff5453653d67e21f827fa
Sarthak Agarwak <agarwalsarthak456@gmail.com>
2022-09-17 21:16:29 +0530

    Added Mongo Connection

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express')
+
 require("dotenv").config()
+require('./config/database').connect()
+const express = require('express')
+const User = require('./model/user')
+
+
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send("<h1>Hello From Groupy Coverage!</h1>")
+})
+
+app.post("/register", async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    if (!(email && password && firstname && lastname)) {
+        res.send(400).send('All fields are required!')
+    }
+    const existingUser = await User.findOne({ email }) // Its a PROMISE
+
+    if (existingUser) {
+        res.send(401).send('User already exists!')
+    }
 })
 
 
